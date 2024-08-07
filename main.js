bcc3c1b0b540e1d311afc9c9102e129b995efbc2
Ahadxcode <ahadk77777@gmail.com>
2024-08-07 14:26:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+        strings:["Frontend Developer", "Electrical and Electronics Engineer " , "Python Programmer"],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:200,
+        loop:true
+});
